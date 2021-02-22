b61713760122cd4dd6db1c8d563e39bb475110ba
Jake Lynn <jklynn9@gmail.com>
2021-02-21 23:33:02 -0500

    docker working finally

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,10 +1,13 @@
 import { createConnection } from "mysql";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const sqlOptions = {
-  host: "localhost",
-  user: "root",
-  password: "goober123",
-  database: "jewlere",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 };
 
 export const sql = createConnection(sqlOptions);
